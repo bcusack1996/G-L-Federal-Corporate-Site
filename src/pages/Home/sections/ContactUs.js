c007afc93e1e97b9d12e556c620b737a6104c2bb
Oleh Smolikevych <olehsjs@gmail.com>
2024-05-13 23:45:54 -0500

    form submission completed

--- src/pages/Home/sections/ContactUs.js
+++ src/pages/Home/sections/ContactUs.js
@@ -1,38 +1,84 @@
-// import { useState } from "react";
-
-// @mui material components
+import React, { useState } from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
-// import Switch from "@mui/material/Switch";
-
-// Material Kit 2 React components
 import MKBox from "components/MKBox";
 import MKInput from "components/MKInput";
 import MKButton from "components/MKButton";
 import MKTypography from "components/MKTypography";
 
 function FormSimple() {
-  // const [checked, setChecked] = useState(true);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-  // const handleChecked = () => setChecked(!checked);
+  const [submitStatus, setSubmitStatus] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(
+      "https://prod-66.westus.logic.azure.com:443/workflows/f933ea14f09f4794b55d34bb27a7ef3a/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VtZztSLE_YrZkxfYyb-NobVC0AG9KLBb-Cb6BLhcGI4",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      }
+    );
+
+    if (response.ok) {
+      console.log("Message sent");
+      setSubmitStatus("Message sent successfully!");
+      // Reset the form
+      setFormData({
+        name: "",
+        email: "",
+        message: "",
+      });
+    } else {
+      console.error("Failed to send message");
+      setSubmitStatus("Failed to send message. Please try again.");
+    }
+  };
 
   return (
     <MKBox component="section" py={12} id="contact">
       <Container>
-        <Grid container item justifyContent="center" xs={10} lg={7} mx="auto" textAlign="center">
+        <Grid container justifyContent="center" xs={10} lg={7} mx="auto" textAlign="center">
           <MKTypography variant="h3" mb={1}>
             Contact Us
           </MKTypography>
         </Grid>
-        <Grid container item xs={12} lg={7} sx={{ mx: "auto" }}>
-          <MKBox width="100%" component="form" method="post" autocomplete="off">
+        <Grid container xs={12} lg={7} sx={{ mx: "auto" }}>
+          <MKBox width="100%" component="form" autoComplete="off" onSubmit={handleSubmit}>
             <MKBox p={3}>
               <Grid container spacing={3}>
                 <Grid item xs={12}>
-                  <MKInput variant="standard" label="Name" fullWidth />
+                  <MKInput
+                    variant="standard"
+                    label="Name"
+                    fullWidth
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                  />
                 </Grid>
                 <Grid item xs={12}>
-                  <MKInput variant="standard" type="email" label="Email Address" fullWidth />
+                  <MKInput
+                    variant="standard"
+                    type="email"
+                    label="Email Address"
+                    fullWidth
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
                 </Grid>
                 <Grid item xs={12}>
                   <MKInput
@@ -41,36 +87,22 @@ function FormSimple() {
                     multiline
                     fullWidth
                     rows={6}
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
                   />
                 </Grid>
-                <Grid item xs={12} alignItems="center" ml={-1}>
-                  {/* <Switch checked={checked} onChange={handleChecked} /> */}
-                  {/* <MKTypography
-                    variant="button"
-                    fontWeight="regular"
-                    color="text"
-                    ml={-1}
-                    sx={{ cursor: "pointer", userSelect: "none" }}
-                    onClick={handleChecked}
-                  >
-                    &nbsp;&nbsp;I agree the&nbsp;
-                  </MKTypography>
-                  <MKTypography
-                    component="a"
-                    href="#"
-                    variant="button"
-                    fontWeight="regular"
-                    color="dark"
-                  >
-                    Terms and Conditions
-                  </MKTypography> */}
-                </Grid>
               </Grid>
-              <Grid container item justifyContent="center" xs={12} my={2}>
+              <Grid container justifyContent="center" xs={12} my={2}>
                 <MKButton type="submit" variant="gradient" color="dark" fullWidth>
                   Send Message
                 </MKButton>
               </Grid>
+              {submitStatus && (
+                <MKTypography variant="body2" color="error" textAlign="center">
+                  {submitStatus}
+                </MKTypography>
+              )}
             </MKBox>
           </MKBox>
         </Grid>
